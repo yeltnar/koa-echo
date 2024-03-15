1ac2e256155a51b302ee778864e1ed478869f542
Drew Brantley <drew@drew-mbp.lan>
2024-03-15 15:30:59 -0500

    add path to echo

--- routers/index.js
+++ routers/index.js
@@ -3,11 +3,12 @@ let fileCmd = require('file-cmd');
 
 let router = new Router();
 
-router.all('/', async function (next) {
+router.all('/*', async function (next) {
     this.body = {
         query: this.request.query,
         body: this.request.body,
         headers: this.request.headers,
+        path: this.request.path,
     };
     await next;
 })
